a32c2dce9bd2bc19fb0a9c3ac7ac73e724228099
Hridoy Banik <hridoy.banik71@gmail.com>
2020-09-30 21:28:09 +0600

    design menu item

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Card, CardBody, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 const MenuItem = (props) => {
-  console.log(props.dish);
-  return <div></div>;
+  return (
+    <div>
+      <Card style={{ margin: "10px" }}>
+        <CardBody>
+          <CardImg
+            style={{ opacity: "0.5" }}
+            width="100%"
+            alt={props.dish.name}
+            src={props.dish.image}
+          />
+          <CardImgOverlay>
+            <CardTitle>{props.dish.name}</CardTitle>
+          </CardImgOverlay>
+        </CardBody>
+      </Card>
+    </div>
+  );
 };
 
 export default MenuItem;
